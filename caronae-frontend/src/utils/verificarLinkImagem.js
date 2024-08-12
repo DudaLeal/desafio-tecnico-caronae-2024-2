8d04cdb36035e3c52b177a4e6aedc455ef94929e
Eduarda Leal <eduardalc@dcc.ufrj.br>
2024-08-11 21:39:28 -0300

    fix: correção para mostrar o status do response

--- caronae-frontend/src/utils/verificarLinkImagem.js
+++ caronae-frontend/src/utils/verificarLinkImagem.js
@@ -1,10 +1,10 @@
 export async function verificarLinkImagem(imageUrl) {
     try {
       const response = await fetch(imageUrl, { method: 'HEAD' });
-      console.log('Status do response:', response.ok); // Mostra o status da resposta
+      console.log('Status do response:', response.status);
       return response.ok;
     } catch (error) {
-      console.error('Erro ao verificar link da imagem:', error); // Mostra o erro ocorrido
+      console.error('Erro ao verificar link da imagem:', error);
       return false;
     }
   }
